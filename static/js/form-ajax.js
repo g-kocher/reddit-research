eb5dc6258bbb812bcc8e883f2b1383bd0842b811
garrett <g.kocher@gmail.com>
2014-05-08 12:49:04 -0700

    add serach cog

--- static/js/form-ajax.js
+++ static/js/form-ajax.js
@@ -1,23 +1,13 @@
-$('#search-form').submit(function(e){
-	e.preventDefault()
-	$('#results').html('');
-	$('#results').load($('input#subreddit').val()+'/'+$('input[name="order"]:checked').val());
-});
-
-
-
-
-
-/*
-$(document).ready(function(){
-	jQuery(function($){
-		$('#submit').click(function(event) {
-			event.preventDefault();
-			alert('working')
-			$.get('/'+$('input#subreddit').val()+'/'+$('#order').val(), function(data){
-				$('#results').html(data);
-			}, 'text');
+$(function(){
+	$('#search-form').submit(function(e){
+		e.preventDefault();
+		$('#results').html('');
+		$("#searching").show();
+		var url = "/"+$('input#subreddit').val()+'/'+$('input[name="order"]:checked').val();
+		$.get(url, function(data){
+			$("#searching").hide();
+			$('#results').html(data);
 		});
+
 	});
 });
-*/
